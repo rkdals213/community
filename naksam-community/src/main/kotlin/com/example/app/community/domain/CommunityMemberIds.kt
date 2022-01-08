6f3daa6c8c2691a5c96031da03f9d0d249e3d8c7
rkdals213 <rkdals213@naver.com>
2022-01-08 18:59:44 +0900

    gg

--- naksam-community/src/main/kotlin/com/example/app/community/domain/CommunityMemberIds.kt
+++ naksam-community/src/main/kotlin/com/example/app/community/domain/CommunityMemberIds.kt
@@ -14,14 +14,12 @@ class CommunityMemberIds(
     @Column(name = "member_id")
     val memberIds: MutableSet<Long> = mutableSetOf()
 ) {
-    fun count() = memberIds.size
-
     fun add(memberId: Long) {
         check(ableToJoin(memberId)) { "이미 가입했거나 최대 가입 가능한 인원을 초과했습니다" }
         memberIds.add(memberId)
     }
 
-    fun ableToJoin(memberId: Long) = memberIds.count() < maxMemberCount && memberIds.notContains(memberId)
+    fun ableToJoin(memberId: Long) = memberIds.size < maxMemberCount && memberIds.notContains(memberId)
 
     fun withdrawal(memberId: Long) {
         check(memberIds.contains(memberId)) { "가입한 회원이 아닙니다" }
